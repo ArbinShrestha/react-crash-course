00c22f908a2fa58e928b3e62ab6e5211003c2fa2
ArbinShrestha <orbeenshr@gmail.com>
2022-08-28 20:54:00 +0545

    learning react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import Header from './components/Header'
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <h2> Hello </h2>
+      <Header  />
     </div>
     
   );
 }
+
 
 export default App;
