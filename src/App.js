af0fa1cf6a0b744efda0ffac3e8651bf4b639c36
ArbinShrestha <orbeenshr@gmail.com>
2022-08-31 09:39:06 +0545

    configuring form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import AddTask from "./components/AddTask"
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -27,7 +29,9 @@ function App() {
 
   // Add task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
   }
 
   // Delete Task
@@ -46,8 +50,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
